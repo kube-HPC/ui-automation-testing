dc8234bf885e50fc5536950657143040260e0e7f
zivglik <zivglik@gmail.com>
2026-07-21 10:47:34 +0300

    feat(tests): add edit algorithm test with description validation

--- tests/algorithms/actions/delete/delete.spec.ts
+++ tests/algorithms/actions/delete/delete.spec.ts
@@ -51,3 +51,32 @@ test("delete algorithm", async ({ page }) => {
     }
   }
 });
+
+test("edit algorithm", async ({ page }) => {
+  const algorithmName = generateTestName("editAlgorithm");
+
+  await createAlgorithm(algorithmName);
+
+  try {
+    await gotoRoot(page);
+    await getSideBarLeftLink(page, NamesLeftLink.ALGORITHMS).click();
+
+    const algorithmRow = hkGridFindRowByColumnText(page, "name", algorithmName);
+    const editButtonInRow = algorithmRow.hkGridGetActionButton("edit");
+    await editButtonInRow.click();
+
+    await page.getByRole("textbox", { name: "Description :" }).click();
+    await page
+      .getByRole("textbox", { name: "Description :" })
+      .fill("test edit");
+
+    await page.getByRole("button", { name: "Save" }).click();
+    await editButtonInRow.click();
+
+    await expect(
+      page.getByRole("textbox", { name: "Description :" }),
+    ).toHaveValue("test edit");
+  } finally {
+    await deleteAlgorithm(algorithmName).catch(console.error);
+  }
+});
